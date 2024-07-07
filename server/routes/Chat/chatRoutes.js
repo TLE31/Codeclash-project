0cf74899dc5aae0c4f57acb927b536e856497234
PriyanshuTiwari <131243619+TLE31@users.noreply.github.com>
2024-07-07 15:35:41 +0530

    Add files via upload

---
+++ server/routes/Chat/chatRoutes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const chatController = require("./../../controllers/Chat/chatController");
+const authController = require("./../../controllers/authController");
+
+const router = express.Router();
+
+router.route("/").post(authController.protect, chatController.acessChat);
+router.route("/").get(authController.protect, chatController.fetchChats);
+router
+  .route("/create-discussion")
+  .post(authController.protect, chatController.createGroupChat);
+
+router
+  .route("/discussion")
+  .get(authController.protect, chatController.getAllDiscussion);
+
+router.route("/vote/:id").post(authController.protect, chatController.doVotes);
+router.route("/slug").post(authController.protect, chatController.findBySlug);
+
+// router.route("/bot").post(authController.protect, chatController.bot);
+router.route("/bot").post(chatController.bot);
+
+// router
+//   .route("/delete-discussion")
+//   .delete(
+//     authController.protect,
+//     authController.restrictTo("admin"),
+//     chatController.deleteDiscussion
+//   );
+
+module.exports = router;
